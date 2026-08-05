63c01c419ac239a841f9db247bc1039206c4ed0b
shyaveashwini <shyaveashwini@gmail.com>
2026-08-05 12:36:44 +0530

    Time: 8 ms (16.29%), Space: 12.3 MB (94.27%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        for i in t:
+            if i not in s:
+                return i
+            s=s.replace(i,"",1)
+        
+
+            
+            
